315fe9e4bbf794d530e70c0d847112b49495cc22
Krystal Gayle <krystal.gayle24@gmail.com>
2015-11-22 01:59:03 -0500

    update inclass testfiles

---
+++ krysgayle_website/inclass_testfiles/inclass19_testfile2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Array Functions </title>
+        <meta http-equiv="content-type" content="text/html"; charset="UTF-8">
+    </head>
+ 
+    <body>
+        
+        <?php
+           /*$names1 = array ("Salima", "Antonio", "Krystal");
+            $names2 = array ("Odeilia", "Alejandro", "Aline");
+            
+            
+            $a = count($names1);
+            $b = count($names2);
+
+            //$a = 10;
+            //$b= 6;
+
+            if ($a < $b)
+            {        
+            echo "Booyah! <br>";    
+            }
+                elseif ($a == $b) {
+                    echo "They are the same";
+            }
+            
+                else {   
+            echo "Oops, a is not greater than b <br>";
+            }*/
+
+            //if ($b > $a)
+            //echo "POOP!";
+
+            $a = 5; 
+            
+        ?>
+        
+            <?php if ($a == 5): ?>
+                
+                <h1>A Equals 5</h1>
+            
+            <?php endif; ?>
+
+
+            
+       
+    
+    </body>
+
+</html>
